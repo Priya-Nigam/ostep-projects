5fc5089976d6355cc3fc85a6b50f2722c36c3eb6
Priya Nigam <priya.nigam@temple.edu>
2020-01-15 16:26:28 -0500

    wcat functional

--- initial-utilities/wcat/wcat.c
+++ initial-utilities/wcat/wcat.c
@@ -17,9 +17,8 @@ int main(int argc, char **argv) {
                 //1 indicates failure 
         }
         //while we aren't at the end of file, get the line and print it
-
-        //first try to print one line 
-        fgets(buff, 255, (FILE*) file);
-        printf("%s",buff);
+        while(fgets(buff, 255, (FILE*) file)) {
+        	printf("%s",buff);
+	}
         fclose(file);       
 } 
