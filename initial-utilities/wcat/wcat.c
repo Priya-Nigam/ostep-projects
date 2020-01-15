e845d3c6a2520bf4aa43d1007cd6dd11b8fa6a2a
Priya Nigam <priya.nigam@temple.edu>
2020-01-15 16:18:40 -0500

    wcat prints one line

--- initial-utilities/wcat/wcat.c
+++ initial-utilities/wcat/wcat.c
@@ -1,23 +1,25 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void) {
-	//buffer buff 
-	char buff[255];
+//take input from command line as file and print it out line by line 
 
-	//open file that user inputed
-	FILE *file = fopen("main.c", "r");
-	//check that file can be opened 
-	if (file  == NULL) {
- 		printf("\nSorry! We cannot open that file\n");
- 		exit(1);
-		//1 indicates failure 
-	}
-	//while we aren't at the end of file, get the line and print it
+int main(int argc, char **argv) {
+        //buffer buff 
+        char buff[255];
 
-	//first try to print one line 
-	fgets(buff, 255, (FILE*) file);
-	printf("%s",buff);
-	fclose(file);	
+        //open file that user inputed
+	//read only 1st file 
+        FILE *file = fopen(argv[1], "r");
+        //check that file can be opened 
+        if (file  == NULL) {
+                printf("\nSorry! We cannot open that file\n");
+                exit(1);
+                //1 indicates failure 
+        }
+        //while we aren't at the end of file, get the line and print it
+
+        //first try to print one line 
+        fgets(buff, 255, (FILE*) file);
+        printf("%s",buff);
+        fclose(file);       
 } 
-
