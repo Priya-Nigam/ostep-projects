ff4fe6bb112909969724b81d8cc8ea5c3d21787d
Priya Nigam <priya.nigam@temple.edu>
2020-01-29 14:10:03 -0500

    edited wzip, triyng to get it to work for examples

--- initial-utilities/wzip/wzip.c
+++ initial-utilities/wzip/wzip.c
@@ -18,50 +18,59 @@ int main(int argc, char **argv) {
 		printf("wzip: file1 [file2 ...]\n");
                 exit(1);
 	}
+	//if you have 2 files and one ends with aaa and next starts with aaa will read as aaaaaa
+	int turnover_count = 0;
+	char turnover_char;
+		
 	for (int i = 1; i < argc; i++) {
 
 		FILE *file = fopen(argv[i], "r");
         	//check that file can be opened 
         	if (file  == NULL) {
-                	printf("wzip: file1 [file2 ...]\n");
+                	printf("wzip: cannot open file\n");
                 	exit(1);
                 	//1 indicates failure 
       		}
         	//while we aren't at the end of file, get the line and compress it
 		//to compress,  when encounter n characters of same type in a row, turn into n and a single instance of that character
-        	while(fgets(buff, buff_size, (FILE*) file)) {
-            		int length = strlen(buff);
-			char *line = malloc(sizeof(char) * (2*length+1));
-                    	int position = 0;
-			char tracker[buff_size];
-			for (int i = 0; i < length-1 ; i++) {
-			//for (int i = 0; i < length; i++) {
-				int count = 1;
-				//count occurence of this char
-				while (i < length -1 && buff[i] == buff[i+1]) {
-					count++;
-					i++;
-					//increment every time we have a letter that is the same as the previous
-				}
-				//count number of occurrences of new character
-				sprintf(tracker, "%d", count);
-				for (int j = 0; *(tracker + j); j++, position++) {
-					line[position] = tracker[j];
-
-				}	
-				line[position++] = buff[i];
-				//line[position] = buff[i];
-				//store number then letter
+        	int count = 0;
+		char position; 
+		//if there is a carryover 
+		if (turnover_count != 0) {
+			count =  turnover_count;
+			position = turnover_char;;
+		} else {
+		//no carryover from previous file
+			count = 1;
+			//initiate counter
+			position = fgetc(file);
+		}
+		while(position != EOF) {
+          
+			char upcoming = fgetc(file);
+			while ((upcoming == position) && (upcoming !=EOF)) {
 			
-			}
-			//null terminator for string
-			line[position] = '\0';
-				         
-                        fwrite(line, strlen(line), 1, stdout);
-			
-             	}
-	
+				//if the next one is also the same char
+				count++;
+				upcoming = fgetc(file);
+			} 	
+			//if we've reached the end of the file 
+		//	if (upcoming != EOF) {
+		//	if (upcoming  == EOF) {
+				//set new position and count to current if new file 
+				printf("%d", count);
+				fwrite(&count,4,1,stdout);
+				fwrite(&position,1,1,stdout);
+				//reset all the counters
+				count = 1;
+				//turnover_count = -1;
+		//	} else {
+				turnover_count = count;
+				turnover_char = position;
+		//	}
+			position = upcoming;
+		}
         	fclose(file); 
 	}      
-	
+	exit(0);	
 } 	
