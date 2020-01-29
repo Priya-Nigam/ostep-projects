6b26eb1a4caa724c25dda6213f44e8c5c679c560
Priya Nigam <priya.nigam@temple.edu>
2020-01-29 15:18:40 -0500

    worked out issues with  wzip and wzip.c

--- initial-utilities/wzip/wzip.c
+++ initial-utilities/wzip/wzip.c
@@ -21,7 +21,8 @@ int main(int argc, char **argv) {
 	//if you have 2 files and one ends with aaa and next starts with aaa will read as aaaaaa
 	int turnover_count = 0;
 	char turnover_char;
-		
+	int count = 0;
+	char position;	
 	for (int i = 1; i < argc; i++) {
 
 		FILE *file = fopen(argv[i], "r");
@@ -33,19 +34,19 @@ int main(int argc, char **argv) {
       		}
         	//while we aren't at the end of file, get the line and compress it
 		//to compress,  when encounter n characters of same type in a row, turn into n and a single instance of that character
-        	int count = 0;
-		char position; 
+        //	int count = 0;
+	//	char position; 
 		//if there is a carryover 
 		if (turnover_count != 0) {
 			count =  turnover_count;
-			position = turnover_char;;
+			position = turnover_char;
 		} else {
 		//no carryover from previous file
 			count = 1;
 			//initiate counter
 			position = fgetc(file);
 		}
-		while(position != EOF) {
+		while((position != EOF)) {
           
 			char upcoming = fgetc(file);
 			while ((upcoming == position) && (upcoming !=EOF)) {
@@ -54,21 +55,28 @@ int main(int argc, char **argv) {
 				count++;
 				upcoming = fgetc(file);
 			} 	
-			//if we've reached the end of the file 
-		//	if (upcoming != EOF) {
-		//	if (upcoming  == EOF) {
-				//set new position and count to current if new file 
-				printf("%d", count);
-				fwrite(&count,4,1,stdout);
-				fwrite(&position,1,1,stdout);
-				//reset all the counters
+			
+			//if the next char isn't new line or the end of a file
+			if (i== (argc-1) || (upcoming != EOF)) {
+		
+                                fwrite(&count,4,1,stdout);
+                                fwrite(&position,1,1,stdout);
+				position = upcoming;
+				//reinitiate turnover_count
 				count = 1;
-				//turnover_count = -1;
-		//	} else {
-				turnover_count = count;
-				turnover_char = position;
-		//	}
-			position = upcoming;
+				turnover_count = 0;				
+				
+			} else {
+			//if you have to get next char from the next file
+				if (upcoming == EOF) {
+				//if at the end of the file, store the count and position and move to next file
+					turnover_count = count; 
+					turnover_char= position;
+				} 
+				position = upcoming;
+			//update the position
+				
+			}
 		}
         	fclose(file); 
 	}      
